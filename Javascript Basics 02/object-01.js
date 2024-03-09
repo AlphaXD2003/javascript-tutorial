a41ddfafc712bf77e19f983fe51f160236283acd
Subhamoy Ghosh <146444663+AlphaXD2003@users.noreply.github.com>
2024-03-09 04:36:18 +0000

    obj 01 done

---
+++ Javascript Basics 02/object-01.js
@@ -0,0 +1,30 @@
+/*
+let obj = {
+        key : value
+}
+obj.key
+*/
+
+let mySymbol1 = Symbol('123')
+let mySymbol2 = Symbol('123')
+
+let obj = {
+    name: 'wribhu',
+    'surname': 'nandi',
+    'full name' : 'wribhu nandi', //full name
+    'mySymbol1' : 'key1',
+    [mySymbol2] : 'key2',
+    isBool : false,
+    number : 123
+}
+
+console.log(obj.name)
+console.log(obj['surname'])
+console.log(obj['full name'])
+
+console.log(obj['mySymbol1'])
+console.log(typeof obj['mySymbol1'])
+
+console.log(obj[mySymbol2])
+console.log(typeof obj[mySymbol2])
+
