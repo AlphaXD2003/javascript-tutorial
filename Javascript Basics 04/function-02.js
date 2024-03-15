81316a0dbb0e4f32acf9ccf1820b3a7efd18524a
Subhamoy Ghosh <146444663+AlphaXD2003@users.noreply.github.com>
2024-03-15 04:58:31 +0000

    arrow function

--- Javascript Basics 04/function-02.js
+++ Javascript Basics 04/function-02.js
@@ -2,5 +2,19 @@ const addNumber = function(num1, num2){
     return num1 + num2
 }
 let x = addNumber(30,40)
-console.log(x)
-console.log(addNumber(20, 30))
+//console.log(x)
+//console.log(addNumber(20, 30))
+
+
+// Scope
+
+
+let a = 20;
+var b = 50;
+function print(a){
+    this.a = 30
+    console.log(a)
+    console.log(this.a)
+}
+print(a)
+console.log(this)
---
+++ Javascript Basics 04/function-03.js
@@ -0,0 +1,57 @@
+//arrow function
+
+function hello1(){
+    console.log('Hello1')
+}
+
+hello1()
+const hello2 = function(){
+    console.log('Hello2')
+}
+
+hello2()
+
+// arrow funcition
+const hello3 = () => {
+    console.log('hello3')
+}
+hello3()
+
+
+const print = (word) => {
+    console.log(`${word}`)
+}
+
+print('wribhu')
+
+const sum1 = (num1, num2) => (
+    num1 + num2
+)
+const sum2 = (num1, num2) => (
+ [ num1 + num2, num1 / num2]  
+)
+
+/*
+
+function sum2(num1, num2){
+    let sum = num1 + num2;
+    let div = num1/num2;
+    return [sum, div]
+}
+
+*/
+
+console.log(sum1(5,6))
+
+let x = sum2(5,6)
+console.log(x[0])
+console.log(x[1])
+
+
+const sum3  = (num1, num2 ) => [num1 + num2, num1 / num2]
+
+console.log (sum3(5,6)[0])
+console.log (sum3(5,6)[1])
+
+const print1 = num1 => console.log(num1)
+print1(1)
